66c289a008c72314a2470ceaabb5247ef47d270f
Alex <atr0011@uah.edu>
2018-09-06 00:11:39 -0500

    Command mode is now working. Added command sic em

--- senpai.js
+++ senpai.js
@@ -1,33 +1,51 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
-
 const config = require("./config.json");
+let stats = JSON.parse(fs.readFileSync("./status.json", "utf8"));
 var collector;
-var chatlog;
-var msg;
-var commanding = false;
+let chatlog;
+let commanding = stats["commanding"];
+console.log("yeet");
 
-if (!commanding){
-	client.on("message", message => {
+client.on("message", message => {
+	if (commanding==false){
 	if(message.author.id !== config.onee) return;
 	if (message.content.includes("senpai?")) { 
 	commanding=true;
-	
 	message.channel.send("Yes Onee?");
 	}
-})
+
 }
- if (commanding && (message.author.id == config.onee)){
-	 collector = channel.createMessageCollector();
-	 client.on("message", message => {
-		 if(message.content.contains("sic em")) {
-			 collector.stop();
-			 collector.on('end', chatlog = collected);
-			 msg = channel.fetchMessage(chatlog.last());
-			 msg => console.log(message.content);
-	 }
- })
+ if(message.author.id==173650807542120448){
+ message.channel.send("Fuck off, Herb");}
+ if (commanding==true){
+	 
+		 if(message.author.id == config.onee){
+		 if(message.content.includes("goodbye")){
+			 commanding = false;
+			 message.channel.send("It's been a pleasure serving you, Onee!");
+		 }
+		if(message.content.includes("sic em")) {
+			message.channel.fetchMessages({limit:1,before:message.id})
+			.then(messages =>{kick(messages)})
+			.catch();
+			console.log(chatlog)
+		//message.channel.send(msg.content);
+			 }
+		 }
  }
-client.login(config.token);
+ 	stats.commanding = commanding;
+	fs.writeFile("./status.json", JSON.stringify(stats), (err) => console.error);
+ })
+function kick(messages){
+	var msg = messages.first();
 	
+	var victim = msg.guild.member(msg.author);
+	console.log(victim);
+	victim.kick();
+	msg.channel.send("Get Cucked " + msg.author.username);
+	
+}
+
+client.login(config.token);
