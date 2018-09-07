e378fe0626cbd342d062a77a57836552f4b56c12
Alex <atr0011@uah.edu>
2018-09-06 22:50:30 -0500

    Worked on more stuff and removed my token so i dont get oofed again.

--- senpai.js
+++ senpai.js
@@ -2,36 +2,45 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 const config = require("./config.json");
+//const response = require("./responses.json");
 let stats = JSON.parse(fs.readFileSync("./status.json", "utf8"));
-var collector;
-let chatlog;
+var onee;
 let commanding = stats["commanding"];
+let defending = stats["defending"];
 console.log("yeet");
 
 client.on("message", message => {
+	var contents = message.content.toLowerCase()
+	if(message.author.id == config.onee){
+		onee = true;
+	}
+	else onee = false;
+	
 	if (commanding==false){
-	if(message.author.id !== config.onee) return;
-	if (message.content.includes("senpai?")) { 
+	if (contents.includes("senpai?")&& onee) { 
 	commanding=true;
 	message.channel.send("Yes Onee?");
 	}
-
-}
+	//else{
+		//for(i = 0;i<response.greetings.length;i++){
+	}
+	
+if(defending){
  if(message.author.id==173650807542120448){
  message.channel.send("Fuck off, Herb");}
+}
+ 
  if (commanding==true){
 	 
 		 if(message.author.id == config.onee){
-		 if(message.content.includes("goodbye")){
+		 if(contents.includes("goodbye")){
 			 commanding = false;
 			 message.channel.send("It's been a pleasure serving you, Onee!");
 		 }
-		if(message.content.includes("sic em")) {
+		if(contents.includes("sic em")) {
 			message.channel.fetchMessages({limit:1,before:message.id})
 			.then(messages =>{kick(messages)})
 			.catch();
-			console.log(chatlog)
-		//message.channel.send(msg.content);
 			 }
 		 }
  }
@@ -47,5 +56,6 @@ function kick(messages){
 	msg.channel.send("Get Cucked " + msg.author.username);
 	
 }
+
 
 client.login(config.token);
